905c47a6ae836bcf1ac2057c035593796ee468c4
Ivan Molera <molera@strands.com>
2018-02-11 23:17:13 +0100

    refactoring

--- StringCalculator/src/main/java/StringCalculator.java
+++ StringCalculator/src/main/java/StringCalculator.java
@@ -1,7 +1,5 @@
 package main.java;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,36 +8,43 @@ public class StringCalculator
 {
     public static final String COMMA_SEPARATOR = ",";
     public static final String NEWLINE_SEPARATOR = "\n";
+    private String numbers;
 
     public int add(String numbers) throws NegativeNotAllowedException {
+        this.numbers = numbers;
         int returnValue = 0;
 
         if(!"".equals(numbers)) {
-            List<String> numbersList = new ArrayList<>();
+            String delimitersRegex = getDelimitersRegex(numbers);
+            String[] splittedString = this.numbers.split(delimitersRegex);
 
-            String regex = COMMA_SEPARATOR + "|" + NEWLINE_SEPARATOR;
+            checkNegativeValues(splittedString);
 
-            if(numbers.startsWith("//")) {
-                String newDelimiter = numbers.substring(2,3);
-                regex += "|" + newDelimiter;
-                numbers = numbers.substring(4, numbers.length());
-            }
-
-            String[] splittedString = numbers.split(regex);
-
-            String negativeValues = null;
-            for (String num : splittedString) {
-                Integer value = new Integer(num);
-                if(value.intValue() < 0) {
-                    negativeValues += " " + num;
-                }
-            }
-
-            if(negativeValues != null) throw new NegativeNotAllowedException("negatives not allowed" + negativeValues);
-
-            numbersList.addAll(Arrays.asList(splittedString));
+            List<String> numbersList = new ArrayList<>(Arrays.asList(splittedString));
             returnValue = numbersList.stream().mapToInt(Integer::parseInt).sum();
         }
         return returnValue;
+    }
+
+    private String getDelimitersRegex(String numbers) {
+        String regex = COMMA_SEPARATOR + "|" + NEWLINE_SEPARATOR;
+
+        if(numbers.startsWith("//")) {
+            String newDelimiter = numbers.substring(2,3);
+            regex += "|" + newDelimiter;
+            this.numbers = numbers.substring(4, numbers.length());
+        }
+        return regex;
+    }
+
+    private void checkNegativeValues(String[] values) throws NegativeNotAllowedException {
+        String negativeValues = null;
+        for (String num : values) {
+            Integer value = new Integer(num);
+            if(value.intValue() < 0) {
+                negativeValues += " " + num;
+            }
+        }
+        if(negativeValues != null) throw new NegativeNotAllowedException("negatives not allowed" + negativeValues);
     }
 }
