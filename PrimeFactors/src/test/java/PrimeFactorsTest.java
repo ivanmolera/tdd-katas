8a26e5c2a0d5d8b3cc08379694140c50eefe9f05
Ivan Molera <molera@strands.com>
2018-05-27 17:20:58 +0200

    PrimeFactors fifth test case

--- PrimeFactors/src/test/java/PrimeFactorsTest.java
+++ PrimeFactors/src/test/java/PrimeFactorsTest.java
@@ -20,19 +20,24 @@ public class PrimeFactorsTest {
     @Test
     public void of_2is2() throws Exception {
         List<Integer> factors = PrimeFactors.of(2);
-        Assert.assertTrue(factors.size() == 1 && factors.get(0) == 2);
+        Assert.assertArrayEquals(new Integer[]{2}, factors.toArray());
     }
 
     @Test
     public void of_3is3() throws Exception {
         List<Integer> factors = PrimeFactors.of(3);
-        Assert.assertTrue(factors.size() == 1 && factors.get(0) == 3);
+        Assert.assertArrayEquals(new Integer[]{3}, factors.toArray());
     }
 
     @Test
     public void of_4is2_2() throws Exception {
         List<Integer> factors = PrimeFactors.of(4);
-        Assert.assertTrue(factors.size() == 2);
         Assert.assertArrayEquals(new Integer[]{2, 2}, factors.toArray());
+    }
+
+    @Test
+    public void of_5is5() throws Exception {
+        List<Integer> factors = PrimeFactors.of(5);
+        Assert.assertArrayEquals(new Integer[]{5}, factors.toArray());
     }
 }
