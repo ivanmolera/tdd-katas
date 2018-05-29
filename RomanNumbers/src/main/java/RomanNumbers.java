f6b3906a549262be3aa4ccef459d3490f4580d53
Ivan Molera <molera@strands.com>
2018-05-29 22:19:41 +0200

    RomanNumbers: basic test cases

--- RomanNumbers/src/main/java/RomanNumbers.java
+++ RomanNumbers/src/main/java/RomanNumbers.java
@@ -7,10 +7,14 @@ import java.util.List;
  * Created by ivanmolera on 28/05/2018.
  */
 public class RomanNumbers {
-    public static List<String> convert(Integer number) throws IllegalArgumentException {
-        if(0 <= number || number > 4999) {
+    public static List<String> convert(int number) throws IllegalArgumentException {
+        if(number <= 0 || number > 4999) {
             throw new IllegalArgumentException();
         }
-        return new ArrayList<>();
+        List<String> romanRepresentation = new ArrayList<>();
+        if(number == 1) {
+            romanRepresentation.add("I");
+        }
+        return romanRepresentation;
     }
 }
--- RomanNumbers/src/test/java/RomanNumbersTest.java
+++ RomanNumbers/src/test/java/RomanNumbersTest.java
@@ -1,6 +1,7 @@
 package test.java;
 
 import main.java.RomanNumbers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
@@ -18,5 +19,11 @@ public class RomanNumbersTest {
     @Test(expected = IllegalArgumentException.class)
     public void of_5000throwsException() {
         List<String> romanNumbers = RomanNumbers.convert(5000);
+    }
+
+    @Test
+    public void of_1isI() {
+        List<String> romanNumbers = RomanNumbers.convert(1);
+        Assert.assertArrayEquals(new String[]{"I"}, romanNumbers.toArray());
     }
 }
